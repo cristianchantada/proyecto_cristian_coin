60ef7be816cef90c660456f3c43d3609598328ad
Cristian Varela Casas <cristianchantada@gmail.com>
2022-10-10 17:35:50 +0200

    Commit inicial con infraestructura mínima y servidor funcionando

---
+++ cristian_coin/routes.py
@@ -0,0 +1,6 @@
+from cristian_coin import app
+from flask import render_template
+
+@app.route("/")
+def index():
+    return "Servidor preparado y listo"
---
+++ main.py
@@ -0,0 +1,1 @@
+from cristian_coin import app
